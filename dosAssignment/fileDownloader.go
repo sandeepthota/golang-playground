950910c68333cdd938c8253a020fcbd28b5cc0a7
Saravanan SJ <sjs7007@gmail.com>
2015-09-21 22:35:59 -0400

    cleaning up comments left

---
+++ dosAssignment/fileDownloader.go
@@ -0,0 +1,137 @@
+/*
+
+Run go get golang.org/x/net/html on command prompt to get html package. 
+
+*/
+
+package main
+
+import (
+	"fmt"
+	"golang.org/x/net/html" //for parsing. 
+	"net/http"              //for crawler
+	"strings"               //for has prefix
+	"sync" //for wait group
+	"os" //creating file
+	"io"
+)
+
+var wg sync.WaitGroup
+
+
+//get href attribute from token
+func getHref(t html.Token) (notPresent bool, href string) {
+	//iterate  over token's attributes till you find an href
+	for _, a := range t.Attr {
+		if a.Key == "href" {
+			href = a.Val
+			notPresent = false
+		}
+	}
+	return
+}
+
+//find links to files which have specified extension
+func findFiles(url string, extension string) { //[reference :  http://schier.co/blog/2015/04/26/a-simple-web-scraper-in-go.html]
+
+	baseURL := url[0 : strings.LastIndex(url, "/")+1] //extract base url of web page
+	//	fmt.Println(baseURL)                              // example : http://www.cise.ufl.edu/class/cis4930fa15idm/notes.html to http://www.cise.ufl.edu/class/cis4930fa15idm/
+	resp, err := http.Get(url) //resp = response, err=error returned if any
+
+	if err != nil {
+		fmt.Println("Error in crawling url : " + url)
+		return
+	}
+
+	b := resp.Body
+	z := html.NewTokenizer(b)
+
+	for {
+		tt := z.Next()
+
+		switch {
+		case tt == html.ErrorToken:
+			//reached end of document
+			return
+		case tt == html.StartTagToken:
+			t := z.Token()
+
+			//check if token is a hyper link tag
+			isAnchor := t.Data == "a"
+			if !isAnchor {
+				continue
+			}
+
+			//extract the linked url in hyperlink tag if present
+			notPresent, url := getHref(t)
+			if notPresent {
+				continue
+			}
+
+			fileExt := url[strings.LastIndex(url, ".")+1 : len(url)] //get file extension if any
+
+			//convert link to aboslute link if relative
+			if !strings.HasPrefix(url, "http:") {
+				//	fmt.Println("Relative link found... Converting to absolute.")
+				url = baseURL + url //example notes/dm3part3.pdf to http://www.cise.ufl.edu/class/cis4930fa15idm/notes/dm3part2.pdf
+			}
+
+			/*switch {
+			case fileExt == "pdf":
+				fmt.Println("PDF Found : " + url)
+
+			case fileExt == "ppt":
+				fmt.Println("PPT Found : " + url)
+
+
+			}*/
+			if fileExt == extension {
+				fmt.Println("File of ." + fileExt + " extension found : " + url)
+				wg.Add(1)
+				go downloadFromUrl(url)
+			}
+			//	fmt.Println(fileExt)
+			//	fmt.Println("URL Found : " + url)
+		}
+	}
+}
+
+func downloadFromUrl(url string) { //reference : https://github.com/thbar/golang-playground/blob/master/download-files.go
+	
+	defer wg.Done()
+
+	tokens := strings.Split(url, "/")
+	fileName := tokens[len(tokens)-1]
+	fmt.Println("Downloading", url, "to", fileName)
+
+	output, err := os.Create(fileName)
+	if err != nil {
+		fmt.Println("Error while creating", fileName, "-", err)
+		return
+	}
+	defer output.Close()
+
+	response, err := http.Get(url)
+	if err != nil {
+		fmt.Println("Error while downloading", url, "-", err)
+		return
+	}
+	defer response.Body.Close()
+
+	n, err := io.Copy(output, response.Body)
+	if err != nil {
+		fmt.Println("Error while downloading", url, "-", err)
+		return
+	}
+
+	fmt.Println(n, "bytes downloaded.")
+}
+
+func main() {
+	//extension := "pdf"
+	//findFiles("http://www.cise.ufl.edu/class/cis4930fa15idm/notes.html", extension)
+	//findFiles("https://www.reddit.com/r/aww", "jpg")
+	fmt.Println(os.Args[1])
+	findFiles(os.Args[1],os.Args[2])	
+	wg.Wait() //wait for all go routines to finish
+}
